89a33a3e033b90ad8fd802f3fd46e2982a5af93c
AnnatarHe <iamhele1994@gmail.com>
2016-11-22 23:22:40 +0800

    add sql file and more comments

--- app/controllers/user.go
+++ app/controllers/user.go
@@ -4,19 +4,30 @@ import (
 	"github.com/revel/revel"
 )
 
+/**
+ * User这个Controller上的
+ */
 type User struct {
 	*revel.Controller
 }
 
+/**
+ * 添加一个用户的方法
+ */
 func (c User) Add() revel.Result {
 
 	return c.RenderJson(map[string]string{"hello": "hello"})
 }
 
+/**
+ * 用户登陆
+ */
 func (c User) Login() revel.Result {
 
-	var uid int
+	var uid, role, school_id, News_id int
+	var name, pwd, avatar string
 	c.Params.Bind(&uid, "uid")
+	c.Params.Bind
 
 	return c.RenderJson(map[string]int{"uid": uid})
 }
