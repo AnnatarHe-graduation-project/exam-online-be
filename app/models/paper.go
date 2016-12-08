b94abf738fd2b6268794df795c43ef45bd731a58
AnnatarHe <iamhele1994@gmail.com>
2016-12-08 21:46:22 +0800

    add hero image

--- app/models/paper.go
+++ app/models/paper.go
@@ -9,6 +9,7 @@ type Paper struct {
 	Title      string
 	Alert      string     // 提示信息，警告信息什么的
 	Score      float32    // 可获得学分数量
+	Hero       string     // 图片
 	QuestionID []Question `gorm:"many2many:paper_has_questions;"`
 	CourseID   []Course   `gorm:"many2many:course_has_papers;"`
 }
