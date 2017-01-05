d2a578a94bd3afb267ee25421a3c7887462ab5ec
AnnatarHe <iamhele1994@gmail.com>
2017-01-05 10:36:53 +0800

    add paper controller and some paper routes

---
+++ app/controllers/paper.go
@@ -0,0 +1,87 @@
+package controllers
+
+import (
+	"github.com/AnnatarHe/exam-online-be/app"
+	"github.com/AnnatarHe/exam-online-be/app/models"
+	"github.com/AnnatarHe/exam-online-be/app/utils"
+	"github.com/revel/revel"
+)
+
+// PaperController: paper that has many questions
+type PaperController struct {
+	*revel.Controller
+}
+
+// Fetch: fetch a paper by paper id
+func (p PaperController) Fetch(pid int) revel.Result {
+	paper := models.Paper{}
+	app.Gorm.Find(&paper, pid)
+
+	return p.RenderJson(utils.Response(200, paper, ""))
+}
+
+// add a paper just not random
+func (p *PaperController) Add() revel.Result {
+	var title, alert string
+	var score float32
+	var questionsID, coursesID []int
+
+	var questions []models.Question
+	var courses []models.Course
+	p.Params.Bind(&title, "title")
+	p.Params.Bind(&alert, "alert")
+	p.Params.Bind(&score, "score")
+	p.Params.Bind(&questionsID, "questions")
+	p.Params.Bind(&coursesID, "courses")
+
+	for _, qid := range questions {
+		question := models.Question{}
+		app.Gorm.Find(&question, qid)
+		questions = append(questions, question)
+	}
+	for _, cid := range courses {
+		course := models.Course{}
+		app.Gorm.Find(&course, cid)
+		courses = append(courses, course)
+	}
+
+	// there should to deal the pic
+	hero := "hero"
+
+	paper := models.Paper{
+		Title:      title,
+		Alert:      alert,
+		Score:      score,
+		Hero:       hero,
+		QuestionID: questions,
+		CourseID:   courses,
+	}
+
+	if err := app.Gorm.Create(&paper).Error; err != nil {
+		return p.RenderError(err)
+	}
+
+	return p.RenderJson(utils.Response(200, paper, ""))
+}
+
+// Random: just for test. it get random question form database
+func (p PaperController) Random(cid int) revel.Result {
+	// the interfacce returned by api should be equal in front-end
+
+	var course models.Course
+	var courses []models.Course
+	// is there should fetch data by cid with sql coded by myself?
+	app.Gorm.Find(&course, cid)
+	courses = append(courses, course)
+
+	paper := models.Paper{
+		Title:      "test random paper",
+		Alert:      "this paper will not get any score",
+		Score:      0.00,
+		Hero:       "",
+		QuestionID: course.QuestionID,
+		CourseID:   courses,
+	}
+
+	return p.RenderJson(utils.Response(200, paper, ""))
+}
